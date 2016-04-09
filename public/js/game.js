83bb92cd8abfb2182f11de1c0cc2c5c657a7dc36
Michael Acker <doby162@gmail.com>
2016-04-09 11:01:47 -0500

    experimental data transfer

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,12 @@
+//here be globals
+var socket, space, player, otherPlayers, currentSpeed = 0, cursors;
+var text = null;
+letGoOfW = 0;
+
+
+
+
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     preload: preload,
     create: create,
@@ -10,8 +19,6 @@ function preload() {
     game.load.spritesheet('ship', 'assets/ship.png', 64, 64);
     game.load.spritesheet('otherPlayer', 'assets/ship.png', 64, 64);
 }
-
-var socket, space, player, otherPlayers, currentSpeed = 0, cursors;
 
 function create() {
     socket = io.connect();
@@ -63,7 +70,7 @@ function onSocketConnected() {
     });
     otherPlayers = [];
 
-    socket.emit('new player', {x: player.x, y: player.y});
+    socket.emit('new player', {x: player.x, y: player.y, text:text});
 }
 
 function onSocketDisconnect() {
@@ -116,6 +123,10 @@ function update() {
         if (otherPlayers[i].alive) {
             otherPlayers[i].update();
             game.physics.arcade.collide(player, otherPlayers[i].player);
+            if (otherPlayers[i].text) {
+                text = otherPlayers[i].text;
+                console.log(otherPlayers[i].text);
+            }
         }
     }
 
@@ -133,7 +144,26 @@ function update() {
         }
     }
     //emote
-
+    if (game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+        letGoOfW = 0;
+        if (text == null) {
+            var style = { font: "32px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: player.width, align: "center", backgroundColor: "#ffff00" };
+            text = game.add.text(0, 0, "- text on a sprite -\ndrag me", style);
+            text.anchor.set(0.5);
+        }
+    } else {letGoOfW++;}
+        
+    if (letGoOfW > 20) {
+        if (text != null) {
+            text.destroy();
+        }
+        text = null;
+    }
+    if (text != null) {
+        text.x = Math.floor(player.x + player.width / 2);
+        text.y = Math.floor(player.y + player.height / 2);
+    }
+    
     game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity);
 
     if (currentSpeed > 0) {
@@ -146,6 +176,7 @@ function update() {
     space.tilePosition.y = -game.camera.y;
 
     if (game.input.activePointer.isDown) {
+        //TODO make this better (bounce around the cursor) (actually teh bounce is feature)
         if (game.physics.arcade.distanceToPointer(player) >= 10) {
             currentSpeed = 300;
             player.rotation = game.physics.arcade.angleToPointer(player);
