ccf3314a903fbbb243783f4918785f61fb16152f
Michael Acker <doby162@gmail.com>
2016-04-09 13:09:42 -0500

    moar code

--- public/js/RemotePlayer.js
+++ public/js/RemotePlayer.js
@@ -1,7 +1,7 @@
 var RemotePlayer = function (index, game, player, startX, startY) {
     var x = startX;
     var y = startY;
-    var text;
+    this.text = 'new';
 
     this.game = game;
     this.health = 3;
